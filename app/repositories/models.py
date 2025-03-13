19315840dbb0a0a9ff06650000db0b2bf90a01a2
yoonjuho <dbswn990@naver.com>
2025-03-14 00:53:27 +0900

    refactor: ERD 모델 변경

--- app/repositories/models.py
+++ app/repositories/models.py
@@ -22,20 +22,19 @@ class Role(PyEnum):
     USER = "USER"
     ADMIN = "ADMIN"
 
-
 class Reservation(Base):
     __tablename__ = "reservations"
     
     id = Column(Integer, primary_key=True, index=True)
     reservation_time_id = Column(Integer, ForeignKey('reservation_times.id'), nullable=False)  # reservation_time 테이블과의 관계
-    customer_id = Column(Integer, ForeignKey('customers.id'), nullable=False)  # customer 테이블과의 관계
+    member_id = Column(Integer, ForeignKey('members.id'), nullable=False)  # member 테이블과의 관계
     count = Column(Integer, nullable=True)  # 예비 참가자 수
     status = Column(Enum(ReservationStatus), default=ReservationStatus.PENDING)  # 상태 (PENDING 기본값)
     created_at = Column(DateTime, default=datetime.datetime.now(kst))
 
-    # 예약 시간과 고객과의 관계 설정
+    # 예약 시간과 멤버와의 관계 설정
     reservation_time = relationship("ReservationTime", back_populates="reservations")
-    customer = relationship("Customer", back_populates="reservations")
+    customer = relationship("Member", back_populates="reservations")
 
 class ReservationTime(Base):
     __tablename__ = "reservation_times"
@@ -47,11 +46,11 @@ class ReservationTime(Base):
     # 예약 시간과 예약 테이블과의 관계 설정
     reservations = relationship("Reservation", back_populates="reservation_time")
 
-class Customer(Base):
-    __tablename__ = "customers"
+class Member(Base):
+    __tablename__ = "members"
     
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=True)
     role = Column(Enum(ReservationStatus), default=Role.USER)  # 상태 (USER 기본값)
-    # 고객과 예약 테이블과의 관계 설정
-    reservations = relationship("Reservation", back_populates="customer")
+    # 멤버와 예약 테이블과의 관계 설정
+    reservations = relationship("Reservation", back_populates="member")
