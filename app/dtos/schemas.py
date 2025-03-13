0480853c7d41dd6c8ca169eb7d29bbe3dcdd838e
yoonjuho <dbswn990@naver.com>
2025-03-14 01:01:21 +0900

    feat: 예약 생성 DTO 구현

--- app/dtos/schemas.py
+++ app/dtos/schemas.py
@@ -3,11 +3,23 @@
 import datetime
 from pydantic import BaseModel
 from typing import Optional
+from app.repositories.models import ReservationStatus
 
 class AvailableReservation(BaseModel):
     id: int
     date_time: datetime
     total_count: int
+
+    class Config:
+        orm_mode = True
+
+class Reservation(BaseModel):
+    id: int
+    member_id: int
+    reservation_time_id: int
+    count: int
+    status: ReservationStatus
+    created_at: datetime
 
     class Config:
         orm_mode = True
